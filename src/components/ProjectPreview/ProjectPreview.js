5c087eeb6eedaea6d01d6e1daacb2d7d74e1346c
thematejsulc <sulc.matej@icloud.com>
2023-05-16 18:31:08 +0200

    feat: Add ProjectPreview component

---
+++ src/components/ProjectPreview/ProjectPreview.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function ProjectPreview() {
+	return (
+		<section className="bg-white">
+			<div className="ph4 ph5-m ph6-l">
+				<img
+					src="https://www.renebieder.com/_next/image?url=https%3A%2F%2Fcdn.fontdue.com%2Fstudio-rene-bieder%2Fimages%2F1454733045365015433%2FStudio-Rene-Bieder-Fonts-In-Use-104-Palmar-Neue-Magnat-Display.jpg%3Fv%3D63847604894&w=1920&q=75"
+					className="db w-100"
+				/>
+				<div class="fl w-100 w-50-l pv3-m pv3-l mb3 mb3-l">
+					<h2 class="lh-title f3 fw4 mt0 mb0">
+						The sudden brilliant idea.
+					</h2>
+					<a className="link hover-black lh-title mid-gray f3 fw3 mt0 mb0" href="#">
+						Beginners and amateurs
+					</a>
+				</div>
+			</div>
+		</section>
+	);
+}
+
+export default ProjectPreview;
