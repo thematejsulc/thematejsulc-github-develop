88f201c0759ac32170e08bf9d5a9e6a0f8bc56f5
thematejsulc <sulc.matej@icloud.com>
2023-05-16 18:08:14 +0200

    style: Update nav element padding in Navbar component
    Adjust the padding of the nav element in the Navbar component to improve
    spacing and alignment. The padding has been modified to ensure proper visual
    balance and consistency within the navigation bar. This change enhances the
    overall aesthetic and user experience of the Navbar component.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,7 +3,7 @@ import styles from "./Navbar.module.css";
 
 function Navbar() {
 	return (
-		<nav className="db dt-l w-100 border-box pa3 ph5-l pv5-l">
+		<nav className="db dt-l w-100 border-box pa4 ph5-m pv4-m ph6-l pv5-l">
 			<a className="db dtc-l v-mid mid-gray link dim w-100 w-25-l tc tl-l mb2 mb0-l f4-l"	href="#" title="Home">
 				Compendium of works up until 2023
 			</a>
