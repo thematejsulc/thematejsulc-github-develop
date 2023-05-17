63e6f239e1771db0a74720dadfc2f58733a039fb
thematejsulc <sulc.matej@icloud.com>
2023-05-17 12:17:08 +0200

    refactor: Remove dummy text

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import ProjectPreview from "./components/ProjectPreview/ProjectPreview";
-import Test from "./components/Test/Test";
 
 function App() {
 	return (
 		<div className="App">
 			<Navbar />
 			<ProjectPreview />
-			Hello, World!
-			<Test />
 		</div>
 	);
 }
